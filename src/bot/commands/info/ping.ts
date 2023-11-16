3860005dc7c94eed0c5133a5575a5b3c84ae78d5
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-11-16 11:52:25 -0300

    Ping incrementado

--- src/bot/commands/info/ping.ts
+++ src/bot/commands/info/ping.ts
@@ -1,9 +1,27 @@
+import { editSucessReply } from "@/bot/utils/discord/editSucessReply";
 import { Command } from "../../structures/Command";
 
 export default new Command({
   name: 'ping',
   description: "replies with pong",
   run: async ({ interaction }) => {
-   await interaction.reply({content:"Pong",ephemeral:true})
+
+    await interaction.deferReply({ ephemeral: true })
+
+    console.log("Pong")
+
+    await editSucessReply({
+      interaction, title: "Pong", fields: [
+        {
+          name: "Bot Latency",
+          value: `${Date.now() - interaction.createdTimestamp}ms`,
+          inline: true
+        },
+        {
+          name: "API Latency",
+          value: `${Math.round(interaction.client.ws.ping)}ms`,
+          inline: true
+        }]
+    })
   },
 })
